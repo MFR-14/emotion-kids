ddb4952c71b5b21ebc68958e7620d6fd9b4675d5
MFR-14 <mfr140293@gmail.com>
2025-12-22 10:23:44 +0700

    Update script.js

--- let startTime = Date.now();  function pilihEmosi(emosi) {   const waktu = ((Date.now() - startTime) /script.js
+++ let startTime = Date.now();  function pilihEmosi(emosi) {   const waktu = ((Date.now() - startTime) /script.js
@@ -1,11 +1,30 @@
+const API_URL = "https://script.google.com/macros/s/AKfycbxalTMNvzoVZ9PeYQ3LfkPhOVaUOh2xDdpy5MWK4envM3Ntsfp2fZIBNgwCUb54qHIr/exec";
+
 let startTime = Date.now();
+const namaAnak = "A01";   // ganti manual bila perlu
+const sesi = "S1";
+let soal = 1;
 
 function pilihEmosi(emosi) {
   const waktu = ((Date.now() - startTime) / 1000).toFixed(2);
 
-  alert(
-    `Emosi: ${emosi}\nWaktu respon: ${waktu} detik`
-  );
-
-  startTime = Date.now();
+  fetch(API_URL, {
+    method: "POST",
+    body: JSON.stringify({
+      nama: namaAnak,
+      sesi: sesi,
+      soal: soal,
+      emosi: emosi,
+      waktu: waktu
+    })
+  })
+  .then(res => res.json())
+  .then(() => {
+    soal++;
+    startTime = Date.now();
+  })
+  .catch(err => {
+    alert("Gagal menyimpan data");
+    console.error(err);
+  });
 }
